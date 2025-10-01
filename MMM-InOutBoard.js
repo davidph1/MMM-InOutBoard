ee3c96ea5e3b284385fe73d2e9b800c4779b3e6a
davidph1 <74727609+davidph1@users.noreply.github.com>
2025-10-01 16:15:10 +0100

    Update MMM-InOutBoard.js

--- MMM-InOutBoard.js
+++ MMM-InOutBoard.js
@@ -24,9 +24,6 @@ Module.register("MMM-InOutBoard", {
   }
   },
 
-  /**
-   * Render the page we're on.
-   */
   getDom() {
     const wrapper = document.createElement("div")
     wrapper.innerHTML = `<b>Title</b><br />${this.templateContent}`
@@ -53,4 +50,66 @@ Module.register("MMM-InOutBoard", {
       this.updatedom();
     }}
   },
+  createTable1: function () {
+        var MMMinOutDiv = document.createElement('div');
+        MMMinOutDiv.classList.add('MMM-inOutBoard');
+        var inOutHeader = document.createElement('div')
+        inOutHeader.className = 'medium'
+        var headerSpan = document.createElement('span')
+        headerSpan.classList.add('medium', 'bright')
+        headerSpan.setAttribute('align', 'right')
+        headerSpan.innerHTML = "In & Out Board"
+        inOutHeader.appendChild(headerSpan)
+        MMMinOutDiv.appendChild(inOutHeader)
+
+        // Create table for first set of data
+        var table1 = document.createElement("table");
+        var table1Header = document.createElement("thead");
+        var table1Body = document.createElement("tbody");
+
+        // Create header row for Name
+        var table1HeaderRow = document.createElement("tr");
+        const nameHead = document.createElement('td');
+        nameHead.setAttribute('align', 'center');
+        nameHead.innerHTML = "Name";
+        table1HeaderRow.appendChild(nameHead)
+        // Create header row for Key
+        const keyHead = document.createElement('td');
+        keyHead.setAttribute('align', 'center');
+        keyHead.innerHTML = 'Key';
+        table1HeaderRow.appendChild(keyHead)
+        // Create header row for indication
+        const whereHead = document.createElement('td');
+        whereHead.setAttribute('align', 'center');
+        whereHead.innerHTML = 'In or Out';
+        table1HeaderRow.appendChild(whereHead)
+        
+        // Append header row to table1 header
+        table1Header.appendChild(table1HeaderRow);
+        // Populate table1 body with data
+        for (let z = 0; z < this.config.numberOfNames; z+=1) {
+            var row = document.createElement("tr");
+            const pos = document.createElement("td");
+            pos.setAttribute('align', 'left');
+            pos.innerHTML = this.config.names[z].name;
+            row.appendChild(pos);
+
+            const teamName = document.createElement('td');
+            teamName.setAttribute('align', 'left');
+            teamName.innerHTML = this.config.names[z].key;
+            row.appendChild(teamName);
+          
+            const teamPoints = document.createElement('td');
+            teamPoints.setAttribute('align', 'right');
+            teamPoints.innerHTML = this.toggleArray[z];
+            row.appendChild(teamPoints);
+            table1Body.appendChild(row);
+        };
+
+        table1.appendChild(table1Header);
+        table1.appendChild(table1Body);
+        MMMinOutDiv.appendChild(table1)
+
+        return MMMinOutDiv;
+    },
 })
